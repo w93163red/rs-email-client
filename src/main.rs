6baba919d2029d4cbc1b39aa10d7e73bfbc7b3b1
LinG <lingxiang.wang@bytedance.com>
2020-04-24 20:01:45 -0400

    first get it run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate error_chain;
+extern crate reqwest;
+extern crate futures;
+extern crate tokio;
+
+use futures::executor::block_on;
+use tokio::prelude::*;
+
+error_chain! {
+   foreign_links {
+      Io(std::io::Error);
+      HttpRequest(reqwest::Error);
+   }
 }
+
+async fn async_main() -> Result<()> {
+   let body = reqwest::get("http://httpbin.org/get").await?.text().await?;
+   println!("Body:\n{}", body);
+
+   Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+   async_main().await;
+}
+

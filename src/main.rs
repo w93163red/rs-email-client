008c55aa52a6c18033fd24e8b6d31bf062cf4847
LinG <lingxiang.wang@bytedance.com>
2020-04-24 22:26:21 -0400

    ready to imap

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,4 @@
-#[macro_use]
-extern crate error_chain;
-extern crate reqwest;
-extern crate futures;
-extern crate tokio;
+fn main() {
 
-use futures::executor::block_on;
-use tokio::prelude::*;
-
-error_chain! {
-   foreign_links {
-      Io(std::io::Error);
-      HttpRequest(reqwest::Error);
-   }
-}
-
-async fn async_main() -> Result<()> {
-   let body = reqwest::get("http://httpbin.org/get").await?.text().await?;
-   println!("Body:\n{}", body);
-
-   Ok(())
-}
-
-#[tokio::main]
-async fn main() {
-   async_main().await;
 }
 

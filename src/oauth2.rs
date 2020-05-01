a12828a6b878d555d9d12ccd408f3508a9c83b7b
LinG <lingxiang.wang@bytedance.com>
2020-05-01 00:27:29 -0400

    get access token

--- src/oauth2.rs
+++ src/oauth2.rs
@@ -1,5 +1,6 @@
 // this file is to get the gmail token
-
+#[allow(non_snake_case)]
+#[allow(unused_imports)]
 use serde::Deserialize;
 
 use percent_encoding::utf8_percent_encode;
@@ -15,6 +16,15 @@ use text_io::read;
 pub struct Auth {
     client_id: String,
     client_secret: String,
+}
+
+#[derive(Deserialize, Debug, Default)]
+pub struct Resp {
+    access_token: String,
+    expires_in: i32,
+    refresh_token: String,
+    scope: String,
+    token_type: String,
 }
 
 pub fn read_authorize_info() -> Result<Auth, Box<dyn Error>> {
@@ -63,22 +73,20 @@ pub async fn get_authorize_tokens(
     authorize_code: &str,
 ) -> Result<String, Box<dyn Error>> {
     let mut params = HashMap::new();
-    params.insert("client_id", string_encode(client_id));
-    params.insert(
-        "redirect_uri",
-        string_encode("urn:ietf:wg:oauth:2.0:oob:auto"),
-    );
-    params.insert("client_secret", string_encode(client_secret));
-    params.insert("code", string_encode(authorize_code));
-    params.insert("grant_type", string_encode("authorization_code"));
-    let account_URL = "https://oauth2.googleapis.com/token";
+    params.insert("client_id", client_id);
+    params.insert("redirect_uri", "urn:ietf:wg:oauth:2.0:oob");
+    params.insert("client_secret", client_secret);
+    params.insert("code", authorize_code);
+    params.insert("grant_type", "authorization_code");
+    let account_url = "https://accounts.google.com/o/oauth2/token";
     let res = reqwest::Client::new()
-        .post(account_URL)
+        .post(account_url)
         .form(&params)
         .send()
         .await?;
-    println!("{:#?}", res.json::<HashMap<String, String>>().await?);
-    Ok(String::new())
+    let res_json = res.bytes().await?;
+    let resp: Resp = serde_json::from_slice(&res_json).unwrap_or(Resp::default());
+    Ok(resp.access_token)
 }
 
 pub fn string_encode(s: &str) -> String {
@@ -93,4 +101,15 @@ pub fn string_encode(s: &str) -> String {
         .add(b'/');
     let params_string = utf8_percent_encode(&s, FRAGMENT).to_string();
     params_string
+}
+
+mod tests {
+    use crate::oauth2::Resp;
+
+    #[test]
+    fn test_serde_json() {
+        let data = b"{\n  \"access_token\": \"123\",\n  \"expires_in\": 3599,\n  \"refresh_token\": \"123\",\n  \"scope\": \"https://mail.google.com/\",\n  \"token_type\": \"Bearer\"\n}";
+        let v: Resp = serde_json::from_slice(data).unwrap();
+        println!("{:?}", v.access_token);
+    }
 }
